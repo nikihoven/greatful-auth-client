2b619a73526913262c604d19c2fee3087ae59108
Nikita <nikki.xovenetti@mail.ru>
2022-04-03 00:57:18 +0300

    Fixed issue with 'createRoot' type

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,8 @@ import { store } from './store'
 import './index.css'
 
 const container = document.getElementById('root')
-if (!container) {
-    throw new Error('Failed to find the root element')
-}
 
-const root = createRoot(container)
+const root = createRoot(container as Element)
 
 root.render(
     <StoreProvider store={store}>
